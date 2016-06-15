706199f7c52b46cedb691d5f61bd706b0d75a45f
杨柳 <yangliu@haohandata.com.cn>
2016-06-15 18:43:53 +0800

    add CUDPOJO

---
+++ matrix_client/src/main/webapp/assets/self-owned/js/generic/generic_cud.js
@@ -0,0 +1,48 @@
+var ServerCUDPOJO = ServerCUDPOJO || {};
+ServerCUDPOJO = {
+    CUDPOJO: {
+        "className": "v2.service.generic.query.entity.ZEntity",
+        "attributes":{
+          "name":"DEMO_TEST_NAME",
+          "category":"DEMO_CATEGORY",
+          "description":"DEMO_DESCRIPTION"
+        }
+    },
+    requestPOJO: {
+        url: $.getServerRoot() + '/service_generic_query/api/cud/add',
+        data: {
+            'queryJson': ''
+        },
+        method: 'POST',
+        successListener: "CUD_ACTION_SUCCESS"
+    },
+    add: function (queryPOJO, successListener) {
+        var Request = ClonePOJO.shallowClone(ServerCUDPOJO.requestPOJO);
+        $(Request).attr("data", {
+            'queryJson': $.toJSON(queryPOJO)
+        });
+        Request.successListener = successListener;
+        var successListener = Request.successListener;
+        $.ajax({
+            url: Request.url,
+            data: Request.data,
+            type: Request.method,
+            contentType: "application/x-www-form-urlencoded;charset=UTF-8",
+            dataType: 'json',
+            success: function (json) {
+                // console.log(json);
+                if (json.hasError) {
+                    console.log("Request get error: " + json.errorMessage);
+                    $.publish("CUD_ACTION_FAILED",json);
+                } else {
+                    $.publish(successListener, json);
+                }
+            },
+            error: function (xhr, status) {
+                console.log('Sorry, there was a problem on CUD Action process!');
+            },
+            complete: function (xhr, status) {
+            }
+        });
+    }
+}
