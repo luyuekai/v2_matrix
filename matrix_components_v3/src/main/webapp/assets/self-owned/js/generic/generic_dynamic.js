2df9b1883fa7cd6a5500af269ef6f77db15c5f81
杨柳 <yangliu@haohandata.com.cn>
2017-07-07 13:25:19 +0800

    s

--- matrix_components_v3/src/main/webapp/assets/self-owned/js/generic/generic_dynamic.js
+++ matrix_components_v3/src/main/webapp/assets/self-owned/js/generic/generic_dynamic.js
@@ -1,1 +1,18 @@
 
+
+function Dynamic_Cloneable_ViewModel() {
+    var self = this;
+
+    self.TYPE_TABLE='TABLE';
+    self.TYPE_CHART='CHART';
+
+    self.type = self.TYPE_TABLE;
+
+    self.vm = ko.observable();//ThinListViewModel
+    self.dom = null;
+    self.dom_id = null;
+
+    self.clone_table = function(){
+
+    }
+}
