7f6829c3ac069776b0cd17a7f0d78bfff4985095
杨柳 <yangliu@haohandata.com.cn>
2017-11-23 21:31:50 +0800

    Merge dashboard-ly-20171110
    Conflicts:
    	generic_dashboard_client/src/main/webapp/module_kit_dashboard.html

--- matrix_components_v3/src/main/webapp/assets/self-owned/js/refine/matrix_util.js
+++ matrix_components_v3/src/main/webapp/assets/self-owned/js/refine/matrix_util.js
@@ -1,3 +1,9 @@
+var Matrix_Util = {
+  gen_id: function() {
+    return Math.round(Math.random() * 100) + "_" + (new Date()).getTime() + "_" + (new Date()).getTime();
+  }
+};
+
 // Matrix DOM Util 通用函数
 // 功能包含：
 //  1，克隆chart
@@ -9,60 +15,56 @@
 //  7，序列化chart option
 //  8，反序列化chart option
 var Matrix_DOM_Util = {
+  DEFAULT_MIN_HEIGHT:160,
+  cache: {
 
-  // 通过chart的option参数，将其还原成chart，放置到指定的div中
-  // 返回值：生成的chart对象，异常则返回空对象
-  // chart_setting_info包含额外的一些附加信息，比如ds和chart type，这两个属性不属于option的直接可以获得属性
-  // chart_setting_info格式:
-  // {
-  //    'chart_ds':...,
-  //    'chart_type':...
-  // }
-  clone_chart: function(destination_div_id, chart_option_info,chart_setting_info) {
-    if (ChartPOJO && destination_div_id) {
-      var option = null;
-      if (typeof chart_option_info === "string") {
-        option = Matrix_DOM_Util.deserialize_chart_option(chart_option_info);
-      } else if (typeof chart_option_info === "object") {
-        option = chart_option_info;
-      }
-      if (option) {
-        var chart = ChartPOJO.reset_chart_option(ChartPOJO.generate_default_chart(destination_div_id), option);
-        if(chart_setting_info){
-          Matrix_Chart_Util.reset_chart_ds(chart,chart_setting_info.chart_ds,chart_setting_info.chart_type);
-        }
-        return chart;
-      }
-    }
-    return null;
   },
-
-  clone_chart_container:function(destination_div_id,chart_option_info,chart_setting_info,original_dom_id,copied_dom_id,copied_chart_id){
-    var tmp_dom_id = copied_dom_id || (new Date()).getTime() + "_dom";
-    var tmp_chart_id = copied_chart_id || (new Date()).getTime() + "_chart";
-    var dom = Matrix_DOM_Util.copy_dom(original_dom_id,tmp_dom_id);
-    var chart_container_div = $(dom).find('.matrix_chart_container');
-    chart_container_div.attr('id', copied_chart_id);
-    chart_container_div.empty();
-    Matrix_DOM_Util.paste_dom(destination_div_id,dom);
-
-    return Matrix_DOM_Util.clone_chart(tmp_chart_id, chart_option_info,chart_setting_info);
+  cache_chart: function(chart_container_id, chart, chart_setting_info) {
+    var cache = {
+      'chart_container_id': chart_container_id,
+      'chart': chart,
+      'chart_setting_info': chart_setting_info
+    };
+    Matrix_DOM_Util.cache[chart_container_id] = cache;
   },
 
   // 拷贝一个dom元素，并将其放置在指定的div容器内
   // 相当于一个完整的COPY+PASTE操作
-  clone_dom: function(destination_parent_div_id, original_dom_id, copied_dom_id) {
-    return paste_dom(destination_parent_div_id, copy_dom(original_dom_id, copied_dom_id));
+  // 支持包含chart的dom
+  clone_dom: function(destination_parent_div_id, original_dom_id) {
+    var dom = Matrix_DOM_Util.copy_dom(original_dom_id);
+    if($(dom).height < Matrix_DOM_Util.DEFAULT_MIN_HEIGHT){
+      $(dom).height(Matrix_DOM_Util.DEFAULT_MIN_HEIGHT);
+    }
+    var chart_container_div = $(dom).find('.matrix_chart_container');
+    var tmp_chart_id = Matrix_Util.gen_id() + "_chart";
+    if (chart_container_div.length>0) {
+      chart_container_div.attr('id', tmp_chart_id);
+      chart_container_div.empty();
+    }
+
+    Matrix_DOM_Util.paste_dom(destination_parent_div_id, dom);
+
+    if (chart_container_div.length>0) {
+      var origin_chart_container_id = $('#'+original_dom_id).find('.matrix_chart_container').attr('id');
+      var cached_chart = Matrix_DOM_Util.cache[origin_chart_container_id];
+      if (!cached_chart) {
+        alert('chart not cached!');
+      } else {
+        Matrix_Chart_Util.gen_chart(tmp_chart_id, cached_chart.chart.getOption(), cached_chart.chart_setting_info);
+      }
+    }
+    return dom;
   },
 
   // 拷贝一个dom元素，并返回其拷贝值
   // 若未指定拷贝后的dom元素id，函数按照当前前端时间戳生成一个默认dom元素id
   // 返回值：生成的拷贝后的dom对象，或空对象
-  copy_dom: function(original_dom_id, copied_dom_id) {
+  copy_dom: function(original_dom_id) {
     if (original_dom_id) {
       // generate div
       var context_div_clone = $('#' + original_dom_id).clone().removeAttr('id');
-      var tmp_id = copied_dom_id || (new Date()).getTime() + "_dom";
+      var tmp_id = Matrix_Util.gen_id() + "_dom";
       context_div_clone.attr('id', tmp_id);
       context_div_clone.css('display', '');
 
@@ -114,41 +116,74 @@ var Matrix_DOM_Util = {
 //没有将chart type放入到chart ds，是因为两者并没有直接的包含关系
 //由于Generic_chart.js存在问题，目前这里只做架构的代码实现，并只支持pie图
 var Matrix_Chart_Util = {
-  reset_chart_ds: function(chart,chart_ds,chart_type) {
-    if(chart && ds){
-      var interval = chart_ds.refresh_interval || 10;
-      setInterval(function(){
-        Matrix_Chart_Util.retrieve_chart_ds(chart,chart_ds,chart_type);
-      },1000*interval);
 
-      Matrix_Chart_Util.retrieve_chart_ds(chart,chart_ds,chart_type);
+  // 通过chart的option参数，将其还原成chart，放置到指定的div中
+  // 返回值：生成的chart对象，异常则返回空对象
+  // chart_setting_info包含额外的一些附加信息，比如ds和chart type，这两个属性不属于option的直接可以获得属性
+  // chart_setting_info格式:
+  // {
+  //    'chart_ds':...,
+  //    'chart_type':...
+  // }
+  gen_chart: function(chart_container_id, chart_option_info, chart_setting_info) {
+    var chart;
+    if (ChartPOJO && chart_container_id) {
+      chart = ChartPOJO.generate_default_chart(chart_container_id);
+      var option = null;
+      if (typeof chart_option_info === "string") {
+        option = Matrix_DOM_Util.deserialize_chart_option(chart_option_info);
+      } else if (typeof chart_option_info === "object") {
+        option = chart_option_info;
+      }
+      if (option) {
+        chart = ChartPOJO.reset_chart_option(chart, option);
+      }
+      if (chart_setting_info) {
+        Matrix_Chart_Util.reset_chart_ds(chart, chart_setting_info.chart_ds, chart_setting_info.chart_type);
+      }
+      if (!$('#' + chart_container_id).hasClass('matrix_chart_container')) {
+        $('#' + chart_container_id).addClass('matrix_chart_container');
+      }
+      Matrix_DOM_Util.cache_chart(chart_container_id, chart, chart_setting_info);
+    }
+    return chart;
+  },
+
+  reset_chart_ds: function(chart, chart_ds, chart_type) {
+    if (chart && ds) {
+      var interval = chart_ds.refresh_interval || 10;
+      setInterval(function() {
+        Matrix_Chart_Util.retrieve_chart_ds(chart, chart_ds, chart_type);
+      }, 1000 * interval);
+
+      Matrix_Chart_Util.retrieve_chart_ds(chart, chart_ds, chart_type);
     }
   },
 
-  retrieve_chart_ds:function(chart,chart_ds,chart_type){
+  retrieve_chart_ds: function(chart, chart_ds, chart_type) {
     var wrapper = {
-      'chart':chart,
-      'ds':chart_ds,
-      'chartType':chart_type
+      'chart': chart,
+      'ds': chart_ds,
+      'chartType': chart_type
     };
     var url = wrapper.ds.ds;
     var rest_mode = wrapper.ds.rest_mode;
     var request_params = wrapper.ds.request_params || null;
-    if(request_params){
+    if (request_params) {
       request_params = JSON.parse(request_params);
     }
-    $.serverRequest(url, request_params, "SUCCESS_LISTENER_DYNAMIC_CHART", "DEFAULT_RETRIEVE_API_FAILED_LISTENER", "DEFAULT_RETRIEVE_API_EXCEPTION_LISTENER", rest_mode, true,wrapper);
+    $.serverRequest(url, request_params, "SUCCESS_LISTENER_DYNAMIC_CHART", "DEFAULT_RETRIEVE_API_FAILED_LISTENER", "DEFAULT_RETRIEVE_API_EXCEPTION_LISTENER", rest_mode, true, wrapper);
   },
 
-  retrieve_chart_ds_listener:function() {
+  retrieve_chart_ds_listener: function() {
     if (arguments && arguments[1]) {
       var server_data = arguments[1].response;
       var ds = arguments[1].addtion.ds;
       var chart = arguments[1].addtion.chart;
       var chart_type = arguments[1].addtion.chartType;
       var tableData;
-      if(ds.json_rule){
-        var tmp = 'server_data.'+ds.json_rule;
+      if (ds.json_rule) {
+        var tmp = 'server_data.' + ds.json_rule;
         server_data = eval(tmp);
         tableData = DataTransferPOJO.serverJsonData2TableData(server_data);
       }
